7108171df987eab24aa1a84ddf669f38e418f66a
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2023-04-20 20:36:27 -0400

    Regra de agrupamento por dia
    Co-authored-by: WesleyLeonardo <WesleyLeonardo@users.noreply.github.com>

--- src/components/GraficoEstrelas/index.jsx
+++ src/components/GraficoEstrelas/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import './index.css'
 import PropTypes from 'prop-types';
+import { Line } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
 
 /**
  * Componente que representa o gráfico de estrelas.
@@ -10,7 +13,64 @@ import PropTypes from 'prop-types';
  *     Além disso, usem dos componentes que forem necessários, para issio importem o pacote junto aos "imports".
  */
 export function GraficoEstrelas(props) {
-  return <div>{'//TODO'}</div>;
+  function obterDia(props, index) {
+    return props.estrelas[index].starred_at.getDay();
+  }
+
+  function obterAno(props, index) {
+    return props.estrelas[index].starred_at.getFullYear();
+  }
+  
+  function obterMes(props, index) {
+    return props.estrelas[index].starred_at.getMonth() + 1;
+  }
+
+  function unique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: true,
+    },
+  };
+  
+  let labels = [];
+  const dias= [];
+  const contagem = [];
+
+  if(props.agrupamento === 'dia'){
+    for (let index = 0; index < props.estrelas.length; index++) {
+      const dia = obterDia(props, index) + "/" + obterMes(props, index) + "/" + obterAno(props, index);
+      dias.push(dia);
+    }
+
+    labels = dias.filter(unique);
+
+    for (let index = 0; index < labels.length; index++) {
+      const repeticao = props.estrelas.filter(estrela => labels[index] === estrela.starred_at).length;
+      contagem.push(repeticao);
+    }
+  }
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: contagem,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+    ],
+  };
+
+  console.log(data);
+ 
+  return <div className='grafico'>
+    <Line options={options} data={data} />
+  </div>;
 }
 
 // Definição dos tipos das propriedades recebidas.
